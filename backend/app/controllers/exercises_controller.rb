df26dde224c955ce59ce9d2d8b39fbda4f306458
ray <rvy5009@gmail.com>
2020-02-11 15:20:21 -0500

    got backend good except regiment delete

--- backend/app/controllers/exercises_controller.rb
+++ backend/app/controllers/exercises_controller.rb
@@ -2,12 +2,11 @@ class ExercisesController < ApplicationController
   before_action :set_regiment
   before_action :set_regiment_exercise, only: [:show, :update, :destroy]
 
-  # GET /todos/:todo_id/items
+
   def index
-    json_response(@regiment.items)
+    json_response(@regiment.exercises)
   end
 
-  # GET /todos/:todo_id/items/:id
   def show
     json_response(@exercise)
   end
@@ -15,20 +14,20 @@ class ExercisesController < ApplicationController
   # POST /todos/:todo_id/items
   def create
     @regiment.exercises.create!(exercise_params)
-    json_response(status: "SUCCESS", message: 'item created successfully.', data: @exercise.name)
+    json_response(status: "SUCCESS", message: 'item created successfully.')
 
   end
 
   # PUT /todos/:todo_id/items/:id
   def update
     @exercise.update(exercise_params)
-    json_response(status: 'SUCCESS', message: 'item updated successfully.', data: @exercise.name)
+    json_response(status: 'SUCCESS', message: 'item updated successfully.')
   end
 
   # DELETE /todos/:todo_id/items/:id
   def destroy
     @exercise.destroy
-    json_response(status: 'SUCCESS', message: 'item deleted successfully.', data: @exercise.name)
+    json_response(status: 'SUCCESS', message: 'item deleted successfully.')
   end
 
   private
