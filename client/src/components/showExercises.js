b361f4eda74e41a91eac6b82853445a5d1249eaf
ray <rvy5009@gmail.com>
2020-02-12 16:13:54 -0500

    shows ok

---
+++ client/src/components/showExercises.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react'
+import { allExercises, verifyUser,deleteRegiment } from "../services/api_helper";
+
+import { Link, withRouter } from "react-router-dom";
+
+class Regiment extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      exercises: []
+    }
+  }
+
+  async componentDidMount() {
+    verifyUser()
+    try {
+    const id = this.props.match.params.id;
+    const results = await allExercises(id)
+    
+    this.setState({
+      exercises: results,
+    })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  delete = async (e, exerciseId) => {
+    e.preventDefault()
+    try {
+      await deleteRegiment(exerciseId)
+      const exercises = this.state.exercises.filter(exercise => exercise.id !== exerciseId)
+      this.setState({
+        exercises
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        
+        {this.state.exercises.map((exercise, key) => (
+        <div key={key}> {exercise.name}
+
+          {/* <button onClick={e => {this.delete(e, exercise.id)}}>
+            Delete
+          </button>
+          <Link to={`/updateExercise/${exercise.id}`} id={exercise.id}>
+            <button>Update</button>
+          </Link> */}
+        </div>))}
+      </div>
+    )
+  }
+}
+
+export default withRouter(Regiment)
+
