620b4577c7ec682cd739a22295b53764e1e6a6e5
ray <rvy5009@gmail.com>
2020-02-12 12:57:52 -0500

    regiments shows now

--- client/src/components/Regiment.js
+++ client/src/components/Regiment.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
-import { allRegiments } from "../services/api_helper";
+import { allRegiments, verifyUser } from "../services/api_helper";
 
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 
 export default class Regiment extends Component {
   constructor(props) {
@@ -10,25 +10,27 @@ export default class Regiment extends Component {
       regiments: []
     }
   }
-  
-  componentDidMount = async () => {
-    // try {
-      const results = await allRegiments();
-      console.log(results);
-      this.setState({
-        regiments: results,
-      });
-    // } catch (e) {
-    //   console.log(e);
-    // }
+
+  async componentDidMount() {
+    verifyUser();
+    try {
+    const results = await allRegiments();
+    
+    this.setState({
+      regiments: results,
+    });
+    } catch (e) {
+      console.log(e);
+    }
   };
 
   render() {
-    console.log(this.state.results);
 
     return (
       <div>
-        hello
+        {this.state.regiments.map((regiment, key) => (
+        <div key={key}> {regiment.title}</div>))}
+       
       </div>
     )
   }
