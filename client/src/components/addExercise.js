2e1335e23866ede67c06871e644c22ef66fc984d
ray <rvy5009@gmail.com>
2020-02-12 16:14:16 -0500

    working on adding

---
+++ client/src/components/addExercise.js
@@ -0,0 +1,94 @@
+import React, { Component } from "react";
+import { verifyUser, addExercise } from "../services/api_helper";
+import { withRouter } from 'react-router-dom'
+
+
+class AddExercise extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      exercises: {
+        name: "",
+        done: "",
+        image: "",
+        instructions: ""
+      }
+    };
+  }
+
+  onChange = e => {
+    const { name, value } = e.target;
+    this.setState({
+      exercises: {
+        ...this.state.exercises,
+        [name]: value
+      }
+    });
+  };
+
+
+  handleSubmit = async e => {
+  
+    e.preventDefault();
+    verifyUser()
+    try {
+      addExercise(this.state.exercises);
+
+    } catch (e) {
+      console.log(e);
+    }
+    this.props.history.push('/regiments')
+  };
+
+  render() {
+    
+    return (
+      <div>
+        <div>
+          <form onSubmit={e => this.handleSubmit(e)}>
+            <input
+              type="text"
+              name="name"
+              placeholder="name"
+              value={this.state.exercises.name}
+              onChange={this.onChange}
+              required
+            />
+            <input
+              type="text"
+              name="image"
+              placeholder="image"
+              value={this.state.exercises.image}
+              onChange={this.onChange}
+              required
+            />
+            <input
+              type="text"
+              name="done"
+              placeholder="done"
+              value={this.state.exercises.done}
+              onChange={this.onChange}
+              required
+            />
+            <input
+              type="text"
+              name="instructions"
+              placeholder="instructions"
+              value={this.state.exercises.instructions}
+              onChange={this.onChange}
+              required
+            />
+            <input type="submit" value="Add Exercise" />
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withRouter(AddExercise);
+
+
+
+
+
