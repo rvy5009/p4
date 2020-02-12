80144d5b589d6d2ae23f5540ca8ea97fa684e6a7
ray <rvy5009@gmail.com>
2020-02-12 12:58:14 -0500

    api is nice too

--- client/src/services/api_helper.js
+++ client/src/services/api_helper.js
@@ -17,25 +17,26 @@ export const loginUser = async (loginData) => {
 
 export const registerUser = async (registerData) => {
 
-    const resp = await api.post('/signup', registerData);
-    api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
-    localStorage.setItem('authToken', resp.data.auth_token);
-    localStorage.setItem('name', resp.data.name);
-    localStorage.setItem('email', resp.data.email);
-    return resp.data.user;
+  const resp = await api.post('/signup', registerData);
+  api.defaults.headers.common.authorization = `Bearer ${resp.data.auth_token}`;
+  localStorage.setItem('authToken', resp.data.auth_token);
+  localStorage.setItem('name', resp.data.name);
+  localStorage.setItem('email', resp.data.email);
+  return resp.data.user;
 
 }
 
 export const verifyUser = () => {
-  const token = localStorage.getItem('authToken');
+  const token = localStorage.getItem('authToken')
   if (token) {
-    api.defaults.headers.common.authorization = `Bearer ${token}`;
+    api.defaults.headers.common.authorization = `Bearer ${token}`
   }
 }
 
 export const allRegiments = async () => {
-  const resp = await api.get('/regiments');
-  return resp;
+  console.log("Hit All Regiments by Rico Corp")
+  const resp = await api.get('/regiments')
+  return resp.data;
 }
 
 // export const postTodo = async (postData) => {
