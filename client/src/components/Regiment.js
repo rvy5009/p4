fd12e56ced5adc1515f5eb0dd014acc5d6a26c6a
ray <rvy5009@gmail.com>
2020-02-12 15:15:39 -0500

    delete and links

--- client/src/components/Regiment.js
+++ client/src/components/Regiment.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import { allRegiments, verifyUser } from "../services/api_helper";
+import { allRegiments, verifyUser,deleteRegiment } from "../services/api_helper";
 
-// import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 
-export default class Regiment extends Component {
+class Regiment extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -12,27 +12,50 @@ export default class Regiment extends Component {
   }
 
   async componentDidMount() {
-    verifyUser();
+    verifyUser()
     try {
-    const results = await allRegiments();
+    const results = await allRegiments()
     
     this.setState({
       regiments: results,
-    });
+    })
     } catch (e) {
-      console.log(e);
+      console.log(e)
     }
-  };
+  }
+
+  delete = async (e, regimentId) => {
+    e.preventDefault()
+    try {
+      await deleteRegiment(regimentId)
+      const regiments = this.state.regiments.filter(regiment => regiment.id !== regimentId)
+      this.setState({
+        regiments
+      })
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   render() {
-
     return (
       <div>
         {this.state.regiments.map((regiment, key) => (
-        <div key={key}> {regiment.title}</div>))}
-       
+        <div key={key}> {regiment.title}
+          <Link to={`/exercises/${regiment.id}`}>
+            <button>Exercises</button>
+          </Link>
+          <button onClick={e => {this.delete(e, regiment.id)}}>
+            Delete
+          </button>
+          <Link to={`/updateRegiment/${regiment.id}`} id={regiment.id}>
+            <button>Update</button>
+          </Link>
+        </div>))}
       </div>
     )
   }
 }
+
+export default withRouter(Regiment)
 
